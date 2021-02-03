09f8ddfa5603a6d3f24d70cfd5a57751abf2eb7e
Zhennan Zhu <48361064+Zhennan-Z@users.noreply.github.com>
2021-02-02 21:30:13 -0500

    Create ui.R

---
+++ ROH/ui.R
@@ -0,0 +1,26 @@
+ui <- fluidPage(
+  titlePanel(("Interface for Interactive Plot of ROH")),
+  sidebarLayout(position = "left",
+                sidebarPanel(id = "sidebar",
+                             fileInput("fileroh", "Choose a roh file"),
+                             fileInput("filerohseg", "Choose a rohseg.gz file"),
+                             fileInput("fileallseg", "Choose a text file with all segements information", accept = "text"),
+                             sliderInput("F_ROH_range", "F_ROH_Range:", min = 0, max = 1,value = c(0,1)),
+                             sliderInput("F_ROH_X_range", "F_ROH_X_Range:",min = 0, max = 1,value = c(0,1)),
+                             width = 3
+                             ),
+                mainPanel(id="main",
+                          tabPanel("Plots", 
+                                   fluidRow(
+                                     splitLayout(style = "border: 1px solid silver:", 
+                                                 plotOutput(outputId = "plot1", click = "plot_click",height = "600px"),
+                                                 plotOutput(outputId = "plot2", height = "600px", width = "100%")
+                                     )),
+                                   fluidRow(
+                                     column(width = 5,
+                                            verbatimTextOutput("click_info"),
+                                            verbatimTextOutput("last_infor")        
+                                     )
+                                   )
+                          )
+                )))
