3bcd332ee8ebf2336bb2df78fc7024dc35cc0811
Zhennan Zhu <48361064+Zhennan-Z@users.noreply.github.com>
2021-02-06 04:47:54 -0500

    Update ui.R

--- ROH/ui.R
+++ ROH/ui.R
@@ -5,22 +5,30 @@ ui <- fluidPage(
                              fileInput("fileroh", "Choose a roh file"),
                              fileInput("filerohseg", "Choose a rohseg.gz file"),
                              fileInput("fileallseg", "Choose a text file with all segements information", accept = "text"),
+                             selectizeInput("ID", "ID",choices =c(Choose='')),
                              sliderInput("F_ROH_range", "F_ROH_Range:", min = 0, max = 1,value = c(0,1)),
                              sliderInput("F_ROH_X_range", "F_ROH_X_Range:",min = 0, max = 1,value = c(0,1)),
                              width = 3
-                             ),
-                mainPanel(id="main",
-                          tabPanel("Plots", 
-                                   fluidRow(
-                                     splitLayout(style = "border: 1px solid silver:", 
-                                                 plotOutput(outputId = "plot1", click = "plot_click",height = "600px"),
-                                                 plotOutput(outputId = "plot2", height = "600px", width = "100%")
-                                     )),
-                                   fluidRow(
-                                     column(width = 5,
-                                            verbatimTextOutput("click_info"),
-                                            verbatimTextOutput("last_infor")        
-                                     )
-                                   )
-                          )
-                )))
+                ),
+                 mainPanel(
+                  tabsetPanel(
+                    tabPanel("Main Plot", 
+                             fluidRow(
+                               splitLayout(style = "border: 1px solid silver:", 
+                                           plotOutput(outputId = "plot1", click = "plot_click",height = "600px"),
+                                           plotOutput(outputId = "plot2", height = "600px", width = "100%")
+                               )),
+                             fluidRow(
+                               column(width = 5,
+                                      verbatimTextOutput("click_info"),
+                                      verbatimTextOutput("last_infor"))
+                             )
+                    ),
+                    
+                    tabPanel("ROH for Selected Study Sample",
+                             plotOutput("plot3",height = "600px", width = "80%"),
+                             dataTableOutput(outputId = "dt1")
+                             )
+                  ))
+))
+
