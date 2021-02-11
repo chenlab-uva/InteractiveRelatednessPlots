cf6f740b5d06b76d24319693e9dd3de4d25b36b5
Zhennan Zhu <48361064+Zhennan-Z@users.noreply.github.com>
2021-02-11 17:02:00 -0500

    Update ui.R

--- IBDSeg/ui.R
+++ IBDSeg/ui.R
@@ -6,30 +6,41 @@ ui <- fluidPage(
                              fileInput("fileibdseg", "Choose a *.segments.gz file"),
                              fileInput("fileallseg", "Choose a text file with all segments information", accept = "text"),
                              sliderInput("IBD1Seg", "IBD1Seg_Range:", min = 0, max = 1,value = c(0,1)),
-                             sliderInput("IBD2Seg", "IBD2Seg_Range:",min = 0, max = 1,value = c(0,1)),
-                             selectizeInput("Pairs_ID1_ID2", "ID1_ID2", choices =c(Choose='')),
+                             sliderInput("IBD2Seg", "IBD2Seg_Range:", min = 0, max = 1,value = c(0,1)),
+                             fluidRow(
+                               column(5, textInput(inputId = "ID1",
+                                                   label = "ID1",
+                                                   value = " ",
+                                                   width = "100px")
+                               ),
+                               column(5, ofset = 3,
+                                      textInput(inputId = "ID2",
+                                                label = "ID2",
+                                                value = "",
+                                                width = "100px")
+                               )),
+                             actionButton(inputId = "EnterIDs", label = "Enter Sample1 ID and Sample2 ID"),
                              width = 2
                 ),
                 mainPanel(
                   tabsetPanel(
-                          tabPanel("Main Plot", 
-                                   fluidRow(
-                                     splitLayout(style = "border: 1px solid silver:", 
-                                                 plotOutput(outputId = "plot1", click = "plot_click", height = "600px"),
-                                                 plotOutput(outputId = "plot2", height = "600px", width = "100%")
-                                     )),
-                                   fluidRow(
-                                     column(width = 8,
-                                            verbatimTextOutput("click_info"),
-                                            verbatimTextOutput("last_infor")        
-                                     )
-                                   )
-                          ),
-                          tabPanel("IBD Segments for the Selected Pair",
-                                   plotOutput("plot3",height = "600px", width = "80%"),
-                                   dataTableOutput(outputId = "dt1")
-                          )
-                          
-                )))
+                    tabPanel("Main Plot", 
+                             fluidRow(
+                               splitLayout(style = "border: 1px solid silver:", 
+                                           plotOutput(outputId = "plot1", click = "plot_click", height = "600px"),
+                                           plotOutput(outputId = "plot2", height = "600px", width = "100%")
+                               )),
+                             fluidRow(
+                               column(width = 8,
+                                      verbatimTextOutput("click_info"),
+                                      verbatimTextOutput("last_infor")        
+                               )
+                             )
+                    ),
+                    tabPanel("IBD Segments for the Selected Pair",
+                             plotOutput("plot3",height = "600px", width = "80%"),
+                             dataTableOutput(outputId = "dt1")
+                    )
+                    
+                  )))
 )
-
