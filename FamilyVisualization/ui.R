24b01e0b9f8142af59121b6b9686d3da5edffb57
Zhennan Zhu <48361064+Zhennan-Z@users.noreply.github.com>
2021-06-02 17:54:59 -0400

    Create ui.R

---
+++ FamilyVisualization/ui.R
@@ -0,0 +1,28 @@
+ui <- fluidPage( 
+  titlePanel(("Interface for Interactive Plot of Family Visualization")),
+  sidebarLayout(position = "left",
+                sidebarPanel(id = "sidebar",
+                             textInput("path", "Path for KING --ibdseg output files", defaultpath),
+                             textInput("prefix", "Prefix for KING --ibdseg inference", "t1dgc"),
+                             textInput("FamilyID", "Family ID for the family to be visualized", "IMCHIP25"),
+                             actionButton(inputId = "EnterFID", label = "Submit"), 
+                             width = 2
+                ),
+                mainPanel(
+                  fluidRow(
+                    fluidRow(
+                      column(6,plotOutput('plot1')),
+                      column(6,plotOutput('plot2',click = "plot_click")),
+                      fluidRow(
+                        column(width = 5,
+                               verbatimTextOutput("click_info"),
+                               verbatimTextOutput("last_infor"))
+                      )
+                    ),
+                    fluidRow(
+                      column(8, plotOutput('plot3', height="600px"))
+                    )   
+                    
+                  )
+                )
+  ))
