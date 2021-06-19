07f96fe08f575237f1fbe60e1ad0ace6815c51f3
Zhennan Zhu <48361064+Zhennan-Z@users.noreply.github.com>
2021-06-19 00:28:12 -0400

    Update global.R

--- FamilyVisualization/global.R
+++ FamilyVisualization/global.R
@@ -1,7 +1,9 @@
+library(shiny)
 library(kinship2)
 library(igraph)
 library(ggplot2)
+library(data.table)
 
 options(scipen = 999)
 options(shiny.maxRequestSize = 30*1024^2)
-defaultpath <-  getwd()
+
