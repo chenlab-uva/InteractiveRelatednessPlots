eb19c63a94e401a80b425031fa3fa407cbec67ee
Zhennan Zhu <48361064+Zhennan-Z@users.noreply.github.com>
2021-06-23 20:51:51 -0400

    Update server.R

--- IBDSeg/server.R
+++ IBDSeg/server.R
@@ -1,64 +1,100 @@
 server <- function(input, output, session) {
   
+  path <- reactiveValues(
+    pth=NULL
+  )
+  
+  prefix <- reactiveValues(
+    name = NULL
+  )
+  
+  observeEvent(input$filechoose,{
+    fullpath <- file.choose()
+    file.base <- basename(fullpath)
+    file.dir <- dirname(fullpath)
+    file.prefix <- gsub(".seg","", file.base)
+    prefix$name <- file.prefix
+    path$pth <- paste(file.dir, file.prefix, sep = "/")
+    updateTextInput(session, inputId = "FID", 
+                    label = paste("Family ID (optional) in", file.prefix, "data"), value = "All")
+  })
+  
+  observeEvent(input$AllFID,{
+    req(path$pth)
+    req(prefix$name)
+    updateTextInput(session, "FID", label = paste("Family ID (optional) in", prefix$name, "data"), value = "All")
+    updateSelectizeInput(session, "IDs", "All inferred relatives", choices =c(Choose=''))
+  })
+  
+  #
+  observeEvent(input$EnterFID, {
+    updateTabsetPanel(session, "inTabset", selected = "panel1")
+  })
+  
+  observeEvent(input$IDs, {
+    updateTabsetPanel(session, "inTabset", selected = "panel2")
+  })
+ 
   infer_df <- eventReactive(input$EnterFID, {
-    req(input$fileinfer)
+    req(path$pth)
     req(input$FID)
-    fileinfer <- input$fileinfer
     if (input$FID == "All"){
-      infer_all <- read.table(fileinfer$datapath, header = TRUE, stringsAsFactors = FALSE) } else {
-        infer_all <- read.table(fileinfer$datapath, header = TRUE, stringsAsFactors = FALSE)
+      infer_all <- read.table(paste0(path$pth, ".seg"), header = TRUE, stringsAsFactors = FALSE) } else {
+        infer_all <- read.table(paste0(path$pth, ".seg"), header = TRUE, stringsAsFactors = FALSE)
         infer_all <- infer_all[infer_all$FID1 == input$FID | infer_all$FID2 == input$FID, ]
       }
     validate(
-      need(nrow(infer_all) > 0, "Please type a valid FID")
+      need(nrow(infer_all) > 0, "Please type a valid Family ID")
     )
     updateSliderInput(session, "IBD1Seg",label = "IBD1Seg_Range",
-                      min = min(infer_all$IBD1Seg), max = max(infer_all$IBD1Seg), 
-                      value = c(min(infer_all$IBD1Seg),max(infer_all$IBD1Seg)))
+                      min = round(min(infer_all$IBD1Seg),4), max = round(max(infer_all$IBD1Seg),4), 
+                      value = c(round(min(infer_all$IBD1Seg),4),round(max(infer_all$IBD1Seg),4)))
     updateSliderInput(session, "IBD2Seg",label = "IBD2Seg_Range",
-                      min = min(infer_all$IBD2Seg), max = max(infer_all$IBD2Seg), 
-                      value = c(min(infer_all$IBD2Seg),max(infer_all$IBD2Seg)))
+                      min = round(min(infer_all$IBD2Seg),4), max = round(max(infer_all$IBD2Seg),4), 
+                      value = c(round(min(infer_all$IBD2Seg),4),round(max(infer_all$IBD2Seg),4)))
     return(infer_all)
   })
   
   segments_df <- reactive({
-    req(input$fileibdseg)
-    fileibdseg <- input$fileibdseg
-    ibdseg <- read.table(fileibdseg$datapath, header = TRUE, stringsAsFactors = FALSE)
+    validate(
+      need(file.exists(paste0(path$pth, ".segments.gz")), paste0(prefix$name, "segments.gz is missing"))
+    )
+    ibdseg <- fread(paste0(path$pth, ".segments.gz"), header = F, data.table = F)
+    colnames(ibdseg) <- c("FID1","ID1","FID2","ID2","IBDType","Chr","StartMB",
+                          "StopMB","StartSNP","StopSNP","N_SNP","Length")
     ibdseg <- ibdseg[, c("ID1", "ID2", "IBDType", "Chr", "StartMB", "StopMB")]
     return(ibdseg)
   })
   
   all_seg_df <- reactive({
-    req(input$fileallseg)
-    fileallseg <- input$fileallseg
-    allseg <- read.table(fileallseg$datapath, header = TRUE)
+    validate(
+      need(file.exists(paste0(path$pth, "allsegs.txt")), paste0(prefix$name, "allsegs.txt is missing"))
+    )
+    allseg <- read.table(paste0(path$pth, "allsegs.txt"), header = TRUE)
     allseg <- allseg[, c("Chr", "StartMB","StopMB")]
     return(allseg)
   })
   
-  filename <- reactive({
-    req(input$fileinfer)
-    fileinfer <- input$fileinfer
-    file_prefix <- gsub(".seg", "", fileinfer$name)
-    return(file_prefix)
-  })
-  
-  
+
   
   observeEvent(input$EnterFID, {
-    req(infer_df)
-    req(input$FID)
     infer_df_FID <- infer_df()
     infer_df_FID <- infer_df_FID[infer_df_FID$FID1 == input$FID | infer_df_FID$FID2 == input$FID, ]
     related_pairs <- paste(infer_df_FID$ID1, infer_df_FID$ID2, sep=" & ")
-    updateSelectizeInput(session, "IDs", label = "IDs", choices = c(Choose='', related_pairs), selected = NULL)
+    new.label <- paste("All inferred relatives in", input$FID)
+    if (input$FID=="All") {
+      updateSelectizeInput(session, "IDs", label = "All inferred relatives", choices = c(Choose=''), selected = NULL)
+    } else {
+      updateSelectizeInput(session, "IDs", label = new.label, choices = c(Choose='', related_pairs), selected = NULL)
+    }
   })
   
   output$plot1 <- renderPlot({
     req(infer_df())
-    req(input$EnterFID)
-    prefix <- filename()
+    #
+    req(segments_df())
+    req(all_seg_df())
+    prefix <- prefix$name
     individuals_all <- infer_df()
     individuals_all <- individuals_all[individuals_all$IBD1Seg >= input$IBD1Seg[1] & individuals_all$IBD1Seg <= input$IBD1Seg[2] & 
                                          individuals_all$IBD2Seg >= input$IBD2Seg[1] & individuals_all$IBD2Seg <= input$IBD2Seg[2],]
@@ -96,17 +132,13 @@ server <- function(input, output, session) {
            col=allcolors, text.col = allcolors, pch = 19, cex = 1.2)
   })
   
-  output$click_info <- renderPrint({
+  output$plot2 <- renderPlot({
     req(input$plot_click)
-    req(infer_df())
-    req(segments_df())
-    req(all_seg_df())
     individuals_all <- infer_df()
     individuals_all <- individuals_all[individuals_all$IBD1Seg >= input$IBD1Seg[1] & individuals_all$IBD1Seg <= input$IBD1Seg[2] & 
                                          individuals_all$IBD2Seg >= input$IBD2Seg[1] & individuals_all$IBD2Seg <= input$IBD2Seg[2],]
     segments <- segments_df()
     all_seg <- all_seg_df()
-    
     chr.num <- ifelse (max(segments$Chr)==23, 23, max(segments$Chr))
     all_seg <- all_seg[all_seg$Chr <=chr.num, ]
     point.index <- which.min((individuals_all$IBD1Seg-input$plot_click$x)^2+(individuals_all$IBD2Seg-input$plot_click$y)^2) 
@@ -115,43 +147,56 @@ server <- function(input, output, session) {
         segments$IBDType <- factor(segments$IBDType, levels = c("IBD0", "IBD1", "IBD2"))
         target.data <- segments[segments$ID1==individuals_all[point.index,"ID1"] & segments$ID2==individuals_all[point.index,"ID2"], ]
       }
-    output$plot2 <- renderPlot({
-      validate(
-        need(nrow(target.data) > 0, "Please select a related pair")
+    validate(
+      need(nrow(target.data) > 0, "Please select a related pair")
+    )
+    Prop.IBD1 <- formatC(individuals_all[point.index, "IBD1Seg"], digits = 3, format = "f")
+    Prop.IBD2 <- formatC(individuals_all[point.index, "IBD2Seg"], digits = 3, format = "f")
+    #
+    theme_set(theme_bw(base_size = 14))
+    g <- ggplot() +
+      geom_rect(data = all_seg, aes(xmin = StartMB, xmax = StopMB, ymin = 0, max = 0.9), fill = 'white', color = "black", size = 0.85) + 
+      geom_rect(data = target.data , aes(xmin = StartMB, xmax = StopMB, ymin = 0, ymax = 0.9, fill = IBDType)) + 
+      geom_rect(data = all_seg, aes(xmin = StartMB, xmax = StopMB, ymin = 0, max = 0.9), color = "black", alpha = 0, size = 0.85) + 
+      scale_fill_manual(values = c("IBD0" = "white", "IBD1" = "dodgerblue2", "IBD2" = "firebrick2"), drop = FALSE) + 
+      facet_grid(Chr ~ .) + scale_x_continuous(expand  = c(0, 0), limits = c(0, NA)) +
+      labs(x = "Position (MB)", y = "", title=substitute(paste("IBD Segments between ", ID1," and ", ID2, " (", pi[1], "=", PropIBD1, ";", pi[2], "=", PropIBD2, ")"),
+                                                         list(ID1 = target.data$ID1, ID2 = target.data$ID2, PropIBD1 = Prop.IBD1, PropIBD2 = Prop.IBD2)))+
+      theme(
+        legend.position = "bottom", legend.key = element_rect(color = "black"),
+        panel.background = element_rect(fill = 'grey80', color = 'grey80'), panel.border = element_blank(),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+        axis.text.y = element_blank(), axis.ticks.y = element_blank()
       )
-      Prop.IBD1 <- formatC(individuals_all[point.index, "IBD1Seg"], digits = 3, format = "f")
-      Prop.IBD2 <- formatC(individuals_all[point.index, "IBD2Seg"], digits = 3, format = "f")
-      #
-      theme_set(theme_bw(base_size = 14))
-      g <- ggplot() +
-        geom_rect(data = all_seg, aes(xmin = StartMB, xmax = StopMB, ymin = 0, max = 0.9), fill = 'white', color = "black", size = 0.85) + 
-        geom_rect(data = target.data , aes(xmin = StartMB, xmax = StopMB, ymin = 0, ymax = 0.9, fill = IBDType)) + 
-        geom_rect(data = all_seg, aes(xmin = StartMB, xmax = StopMB, ymin = 0, max = 0.9), color = "black", alpha = 0, size = 0.85) + 
-        scale_fill_manual(values = c("IBD0" = "white", "IBD1" = "dodgerblue2", "IBD2" = "firebrick2"), drop = FALSE) + 
-        facet_grid(Chr ~ .) + scale_x_continuous(expand  = c(0, 0), limits = c(0, NA)) +
-        labs(x = "Position (MB)", y = "", title=substitute(paste("IBD Segments between ", ID1," and ", ID2, " (", pi[1], "=", PropIBD1, ";", pi[2], "=", PropIBD2, ")"),
-                                                           list(ID1 = target.data$ID1, ID2 = target.data$ID2, PropIBD1 = Prop.IBD1, PropIBD2 = Prop.IBD2)))+
-        theme(
-          legend.position = "bottom", legend.key = element_rect(color = "black"),
-          panel.background = element_rect(fill = 'grey80', color = 'grey80'), panel.border = element_blank(),
-          panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
-          axis.text.y = element_blank(), axis.ticks.y = element_blank()
-        )
-      print(g)
-    })
-    output$dt1 <- renderDataTable({
-      validate(
-        need(nrow(target.data) > 0, "Please select a related pair")
-      )
-      target.data
-    })
+    print(g)
+  })
+  
+  output$dt1 <- renderDataTable({
+    req(input$plot_click)
+    individuals_all <- infer_df()
+    individuals_all <- individuals_all[individuals_all$IBD1Seg >= input$IBD1Seg[1] & individuals_all$IBD1Seg <= input$IBD1Seg[2] & 
+                                         individuals_all$IBD2Seg >= input$IBD2Seg[1] & individuals_all$IBD2Seg <= input$IBD2Seg[2],]
+    segments <- segments_df()
+    all_seg <- all_seg_df()
+    chr.num <- ifelse (max(segments$Chr)==23, 23, max(segments$Chr))
+    all_seg <- all_seg[all_seg$Chr <=chr.num, ]
+    point.index <- which.min((individuals_all$IBD1Seg-input$plot_click$x)^2+(individuals_all$IBD2Seg-input$plot_click$y)^2) 
+    if (!(abs(individuals_all[point.index, "IBD1Seg"]-input$plot_click$x) <= 0.01 & abs(individuals_all[point.index,"IBD2Seg"]-input$plot_click$y) <= 0.01)) {
+      target.data <- NULL } else {
+        segments$IBDType <- factor(segments$IBDType, levels = c("IBD0", "IBD1", "IBD2"))
+        target.data <- segments[segments$ID1==individuals_all[point.index,"ID1"] & segments$ID2==individuals_all[point.index,"ID2"], ]
+      }
+    validate(
+      need(nrow(target.data) > 0, "Please select a related pair")
+    )
+    target.data
   })
   
   output$plot3 <- renderPlot({
     req(input$FID)
     req(input$IDs)
-    req(infer_df())
-    req(all_seg_df())
+    #req(infer_df())
+    #req(all_seg_df())
     individuals_all <- infer_df()
     all_seg <- all_seg_df()
     all_seg_gz <- segments_df()
@@ -193,7 +238,6 @@ server <- function(input, output, session) {
   })
   
   output$dt2 <- renderDataTable({
-    req(input$fileibdseg)
     req(input$IDs)
     pairs <- input$IDs
     pair1 <- unlist(strsplit(pairs, " & "))[1]
